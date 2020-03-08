292f34d5e6eb5beeeb5a908ebe3531a43ee85bd6
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2020-03-07 18:17:06 -0700

    tools to identify cell membership of particles

---
+++ fipy_particle_find.py
@@ -0,0 +1,77 @@
+import fipy
+import gen_mesh
+import utils
+
+import numpy as np
+from matplotlib import pyplot
+
+try:
+    import cmocean
+    my_cm = cmocean.cm.haline
+except:
+    my_cm = pyplot.cm.viridis
+#
+
+s = 5
+pts = np.array([ [np.cos(th), np.sin(th)] for th in 2*np.pi/s*np.arange(s) ])
+
+cellSize = 0.1
+
+mesh = gen_mesh.gen_mesh(pts,cellSize)
+
+flow = fipy.CellVariable(name = r"$u$ (flow)", mesh=mesh, value=0.)
+
+flow.constrain(0., mesh.exteriorFaces) # dirichlet boundary condition
+
+forcing = fipy.CellVariable(mesh=mesh, name='pressure gradient', value=2.)
+
+eq = (-fipy.DiffusionTerm(coeff=1.) == forcing) # set up poisson eqn
+eq.solve(var=flow)
+
+# visualize
+fig,ax = pyplot.subplots(1,1, figsize=(6,6))
+
+utils.vis_fe_2d(flow, cbar=False, cmap=my_cm, ax=ax, edgecolor='w', linewidth=0.1)
+
+# Throw some particles in and identify the cells they belong to.
+ntp = 1000
+test_pts = np.random.randn(ntp,2)
+
+# Identify point cell locations.
+mask,idx = utils.locate_cell(test_pts, mesh)
+
+
+# for some reason, there is a delay in updating the facecolors 
+# within the script; at least when working with ipython.
+# I've thrown in a pyplot.pause(0.5) to try to get around this; 
+# seems to work, but I doubt this is a robust fix.
+fig.show()
+pyplot.ion()
+
+# 
+pyplot.pause(0.1)
+
+# color the cells each of the points belongs to.
+fcs = ax.collections[0].get_facecolors()
+
+for m,ii in zip(mask,idx):
+    if not m:
+        # not in interior; skip
+        continue
+    else:
+#        fcs[ii] = pyplot.cm.magma(np.random.rand())
+        fcs[ii] = [0.5,0,0,1]
+#
+
+# update facecolors
+ax.collections[0].set_facecolors(fcs)
+
+# conveniently, the mask can be used to visualize interior/exterior points too.
+ax.scatter(test_pts[:,0], test_pts[:,1], c=mask, cmap=pyplot.cm.Greys_r, s=5, alpha=0.5)
+
+ax.axis('square')
+ax.set_xlim([-1.2,1.2])
+ax.set_ylim([-1.2,1.2])
+
+fig.tight_layout()
+
--- utils.py
+++ utils.py
@@ -106,8 +106,8 @@ def vis_fe_2d(fipy_cell_var, cbar=True, **kwargs):
     
     triobj = ax.tripcolor(
         triang, 
-        facecolors=fipy_cell_var.value, 
-        cmap=mycm, 
+        facecolors = fipy_cell_var.value, 
+        cmap = mycm, 
         vmin = np.nanmin( fipy_cell_var.value ),     # doesn't handle NaN well
         vmax = np.nanmax( fipy_cell_var.value ),
         **kwargs
@@ -124,4 +124,176 @@ def vis_fe_2d(fipy_cell_var, cbar=True, **kwargs):
         return
     #
 #
+
+def locate_cell(points, gmesh):
+    '''
+    Given a gmesh object (collection of vertices, faces, cells), 
+    generate a collection of pointers indicating which cell each 
+    point lies in. Degenerate cases are not given any special consideration.
+    
+    Input:
+        points: numpy array of shape (2,) or (n,2); either a singleton 
+            or a collection of points in 2D organized by row. 
+        gmesh: a gmesh object which is a triangularization of a domain. 
+            You may also input a CellVariable; the mesh for that variable 
+            contains the same information and is used internally.
+            
+    Outputs: 
+        mask: boolean array shape (n,) indicating whether the points 
+            lie in *any* of the triangles. You should use this as a 
+            preprocessing step to handle exceptions, etc.
+        idx: Either an integer, or a numpy integer array shape (n,) 
+            indicating which cell one or more points lie within.
+            Note that pointers for which mask==False are nonsense 
+            (the mask indicates they are outside the array, so not handled.)
+    '''
+    import numpy as np
+    import fipy
+    import utils
+    
+    # Make sure the mesh is soemthing we know we can work with.
+    if isinstance(gmesh, fipy.variables.cellVariable.CellVariable):
+        mg = gmesh.mesh
+    elif isinstance(gmesh, fipy.meshes.gmshMesh.Gmsh2D):
+        mg = gmesh
+    else:
+        raise Exception('Cannot handle input mesh of type %s'%str(type(gmesh)))
+    #
+    
+    pshape = np.shape(points)
+    if pshape==(2,):
+        singleton = True
+        n=1
+        pts = [points]
+    elif len(pshape)==2 and pshape[1]==2:
+        singleton = False
+        n = pshape[0]
+        pts = points
+    else:
+        raise Exception('Cannot handle input points of shape ', np.shape(points))
+    #
+    
+    #########
+    #
+    # preconstruct coefficient matrices for the mesh.
+    #
+    # TODO - class which precomputes these outside this function.
+    # TODO - get rid of this "np.unique" and figure out why the faces have 
+    # inconsistent ordering around each triangle which makes this a pain.
+    vci = [np.unique(mg.faceVertexIDs[:,ci]) for ci in mg.cellFaceIDs.T]
+    X = mg.vertexCoords[0,vci]
+    Y = mg.vertexCoords[1,vci]
+    
+    # TODO - repackage everything in a nicer way that I'm not constantly 
+    # doing transposes, elementwise operations, etc.
+    #Cmats = np.array([utils._get_cell_coeffs(np.vstack([xi,yi]).T) for xi,yi in zip(X,Y)])
+    
+    ncell = len(vci)
+    
+    # existing code is set up to loop over *cells*, not points.
+    # so, the algorithm will look like
+    # 1. Get collection of undecided particles (monotone decreasing over loops)
+    # 2. Evaluate at current cell
+    # 3. Update associated idx entries
+    # 4. If all idx have been evaluated, exit; else go to 1.
+    # 5. If all cells have been evaluated, mark any remaining points as np.nan
+    #    (they do not lie within the mesh).
+    active_pt_mask = np.ones(n, dtype=bool)
+    idx = np.zeros(n, dtype=int)
+    
+    for i in range(ncell):
+        Cmat = utils._get_cell_coeffs(np.vstack([ X[i],Y[i] ]).T)
+        
+        # get the active set of points
+        pt_subset = points[active_pt_mask]
+        pt_subset_ptr = np.where(active_pt_mask)[0]
+        
+        # get which points lie in the current cell
+        flags = utils._in_Carr(pt_subset, Cmat)
+        # update index
+        idx[pt_subset_ptr[flags]] = i
+        # update active point mask with points accounted for
+        active_pt_mask[pt_subset_ptr[flags]] = False   # god no
+        
+        if any(active_pt_mask):
+            # if any points have yet to be assigned, continue
+            continue
+        else:
+            # all points have been accounted for; we can break.
+            break
+    #
+    
+    # check for any points which haven't been assigned a cell; mark as NaN
+#    idx[active_pt_mask] = np.nan
+    interior_mask = np.logical_not(active_pt_mask)
+    
+    # done - return a scalar if a singleton was fed; else the idx array.
+    if singleton:
+        return interior_mask[0],idx[0]
+    else:
+        return interior_mask,idx
+#
+
+# unneeded for algorithms below
+#
+#def l(xv,yv,coeffs):
+#    return coeffs[0] + coeffs[1]*xv + coeffs[2]*yv
+
+def _get_cell_coeffs(point_coll):
+    '''
+    Given a triangle parameterized by the coordinates of 
+    its vertices, construct the coefficient matrix needed 
+    to evaluate whether a point is in the interior.
+    
+    It's expected this will be used in conjunction with in_interior().
+    This matrix does not have much use otherwise.
+    
+    Inputs:
+        point_coll: numpy array shape (3,2)
+    Outputs:
+        coeff_arr: numpy array shape (3,3)
+    '''
+    import numpy as np
+    
+    coeff_arr = np.zeros((3,3))
+    for i,o in enumerate( [[0,1,2],[1,2,0],[0,2,1]] ):
+        pc = point_coll[o]
+        det0 = np.linalg.det(pc[[0,1]])
+        det1 = np.linalg.det(pc[[0,2]])
+        det2 = np.linalg.det(pc[[1,2]])
+        detA = det0 - det1 + det2
+        c0 = det0/detA
+        c1 = -(pc[1,1] - pc[0,1])/detA
+        c2 = (pc[1,0] - pc[0,0])/detA
+        coeff_arr[i] = [c0,c1,c2]
+    #
+    return coeff_arr
+#
+
+def _in_Carr(point, Carr):
+    '''
+    Given one or more points, evaluate whether each point lies in the 
+    interior of a given triangle parameterized by its coefficient array, 
+    constructed by get_cell_coeffs().
+    
+    Inputs:
+        point: numpy array shape (2,) or (n,2)
+        Carr: numpy array shape (3,3)
+    Outputs:
+        flags: boolean numpy array of shape (n,) indicating 
+            whether each point lies in the given triangle.
+            Note in the singleton case this is an *array* of shape (1,), 
+            not a scalar boolean.
+    '''
+    import numpy as np
+    if len(np.shape(point))==1:
+        point = [point] # make 2d array if given a singleton
+    #
+    
+    Pall = np.array( [ [1.,p[0],p[1]] for p in point] )
+    flags = np.all( np.dot(Carr, Pall.T) >= 0, axis=0)
+    return flags
+#
+
+
 
