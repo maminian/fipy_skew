0c5840fa0f63d7713e68f210299b6e7358c7a3fb
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2022-02-27 00:30:49 -0800

    solid image to coordinates

---
+++ demo10.py
@@ -0,0 +1,51 @@
+# Purpose:
+#
+# Investigate pulling pixels from a hand drawn png.
+#
+
+from matplotlib import pyplot
+import numpy as np
+import utils
+
+from PIL import Image
+
+with Image.open('duck.png') as f:
+    img = np.asarray(f)
+
+#img = img[::-1,:,0].T
+img = img[:,:,0]>0
+img = np.array(img, dtype=int)
+
+border_img = np.gradient(img, axis=0)!=0
+border = np.where(border_img)
+
+coords = np.vstack(border[::-1]).T
+
+# TODO: ordering based on nearest neighbors.
+#o = utils.anticlockwise_order(coords)
+#coords = coords[o]
+from sklearn import metrics
+D = metrics.pairwise_distances(coords)
+keeps = np.zeros(len(coords), dtype=bool)
+keeps[0] = True
+for i in range(1,len(coords)):
+    if not any(np.logical_and(D[i][keeps]>0, D[i][keeps]<10)):
+        keeps[i] = True
+#
+coords = coords[keeps]
+o = utils.anticlockwise_order(coords)
+coords = coords[o]
+o = utils.neighbor_order(coords,10)
+coords = coords[o]
+
+if __name__ == "__main__":
+    from matplotlib import pyplot
+
+    fig,ax = pyplot.subplots(1,1)
+    ax.imshow(img)
+    
+    ax.plot(coords[:,0], coords[:,1], c='k', lw=3)
+    
+    fig.show()
+    pyplot.ion()
+
