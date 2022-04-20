e35b1715a8b9a448e06e9332ce71a617bf58bb2d
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2022-04-19 22:25:51 -0700

    renaming for better tracking

--- demo14.py
+++ demo14.py
@@ -17,16 +17,40 @@ import pandas
 
 import multiprocessing
 
+NPROC = 4
 
 #######
 
 def process_domain(inputs):
     i,name,pts = inputs
-
-    asymptotics = utils.Sk_Asymptotics(pts, cell_size_rel=0.005)
-    result = asymptotics.return_statistics()
-    print(i, name)
-    return [i,name] + list(result)
+    
+    # cell_size_rel seems to not do what it ought to, and 
+    # results in enormous triangulations for 1/lambda domains order 1 (near squares).
+    # just do a dirty hack here to address, and see what happens.
+    aratio_inv = pts[-1][0]  # 1/lambda
+    lam = 1/aratio_inv
+    q = pts[-2][0]/pts[-1][0]
+    
+    if aratio_inv < 5:
+        cell_size_rel=0.020304
+    else:
+        cell_size_rel=0.0050607
+        
+    # back to regularly scheduled programming
+    try:
+        # TODO: weird magic numbers making a problem down the line with 
+        # singular matrices. consider some kind of random perturbation, 
+        # or repeated retries thereof.
+        asymptotics = utils.Sk_Asymptotics(pts, cell_size_rel=cell_size_rel)
+        result = asymptotics.return_statistics()
+        outputs = [i,name] + list(result)
+    except:
+        print(i, name, lam, q)
+        print('\tREPORTS FAILURE OH NO' )
+        
+        outputs = [i,name] + [np.nan for _ in range(7)] # number of outputs is 7 as of Apr 19 2022
+    print(i, name, lam, q)
+    return outputs
 ##################
 
 
@@ -67,20 +91,22 @@ def generate_trapezoid(lam,q):
 #############
 
 # **must** Initialize pool after defining functions! 
-p = multiprocessing.Pool(4)
+p = multiprocessing.Pool(NPROC)
 
 
 ##
 
-aratios = np.linspace(0,1,21)[1:]
+aratios = np.linspace(0,1,51)[1:]
+eccentricities = np.linspace(0,1,51)
+
 #inputs = [ [i,'ellipse_%s'%str(i).zfill(4),generate_ellipse(a)] for i,a in enumerate(aratios)]
 #inputs = [ [i,'ellipse_%s'%str(i).zfill(4),generate_rectangle(a)] for i,a in enumerate(aratios)]
 inputs = []
 import itertools
 lams = []
 qs = []
-for i,pair in enumerate(itertools.product( aratios, np.linspace(0,1,21) )):
-    inputs.append([i, 'trapezoid_%s'%str(i).zfill(4), generate_trapezoid(*pair)])
+for i,pair in enumerate(itertools.product( aratios, eccentricities )):
+    inputs.append([i, 'trapezoid_%s'%str(i).zfill(8), generate_trapezoid(*pair)])
     lams.append(pair[0])
     qs.append(pair[1])
 outputs = p.map(process_domain, inputs)
@@ -95,4 +121,7 @@ df = pandas.DataFrame(
 
 df['lambda'] = lams
 df['q'] = qs
+
+if False:
+    df.to_csv('trapezoid_asymptotics.csv', index=None)
 
