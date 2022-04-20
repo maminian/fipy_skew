53b90ecb51e2d46818ebc5f5d84cccd21f67502e
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2022-04-19 22:18:51 -0700

    compromise on hatching, and polish

--- analysis/csf2.py
+++ analysis/csf2.py
@@ -1,52 +1,66 @@
 import pandas
-from matplotlib import pyplot
+from matplotlib import pyplot, tri
 import numpy as np
 import datetime
 
 pyplot.style.use('ggplot')
 
+
 fig,ax = pyplot.subplots(1,1, figsize=(8,8), constrained_layout=True)
 
-df = pandas.read_csv('../cross_section_features/trapezoid_asymptotics2.csv')
+color_g = '#55f'
+color_l = '#0ff'
+HATCHME = False # hatches are misbehaving.
+
+#####3
+
+data_file = '../cross_section_features/trapezoid_asymptotics_march12_2.csv'
+df = pandas.read_csv(data_file)
+
+# Insertion of triangulation made beforehand.
+triang = tri.Triangulation(df['lambda'].values, df['q'].values)
+
+region1 = np.logical_and(-0.1<df['skew_g'],df['skew_g']<0.1)
+region2 = np.logical_and(-0.1<df['skew_l'],df['skew_l']<0.1)
 
 # contours
 
-skg_contours = ax.tricontour(df['lambda'],df['q'],df['skew_g'], 
-levels=[-0.1,0,0.1], vmin=-0.15,vmax=0.15, 
-colors='k', linewidths=3)
+skg_contours = ax.tricontour(triang,df['skew_g'], levels=[-0.1,0,0.1], vmin=-0.15,vmax=0.15, colors='k', linewidths=3, zorder=100)
 
-skl_contours = ax.tricontour(df['lambda'],df['q'],df['skew_l'], 
-levels=[-0.1,0,0.1], vmin=-0.15,vmax=0.15, 
-colors='k', linewidths=3)
+skl_contours = ax.tricontour(triang,df['skew_l'], levels=[-0.1,0,0.1], vmin=-0.15,vmax=0.15, colors='k', linewidths=3, zorder=100)
 
 ##
 
 # regions
 
-ax.tricontourf(df['lambda'],df['q'],df['skew_g'], 
-levels=[-0.1,0,0.1], vmin=-0.15,vmax=0.15, 
-colors='#008', alpha=0.4)
+ax.tricontourf(df['lambda'],df['q'],df['skew_g'], levels=[-0.1,0,0.1], vmin=-0.15,vmax=0.15, colors=color_g, alpha=1,zorder=0)
 
-ax.tricontourf(df['lambda'],df['q'],df['skew_l'], 
-levels=[-0.1,0,0.1], vmin=-0.15,vmax=0.15, 
-colors='#800', alpha=0.4)
+ax.tricontourf(df['lambda'],df['q'],df['skew_l'], levels=[-0.1,0,0.1], vmin=-0.15,vmax=0.15, colors=color_l, alpha=0.5,zorder=10)
+
+if HATCHME:
+    #
+    # with hatches...
+
+    # TODO: hatches don't seem to respect the linear interpolation 
+    # used by the rest of the contour tools. Can I get this to work?
+
+    # TODO: bizarre behavior with tricontourf hatches being dependent on middle value 
+    # in "levels" parameter. Does not align precisely with interpolated contours 
+    # produced by tricontour. (likely due to the values region1/region2)
+    fuzzy_thresh = 0.3
+    ax.tricontourf(triang, region1, levels=[0,fuzzy_thresh,2], colors=None, hatches=['','\\\\'], alpha=0)
+    ax.tricontourf(triang, region2, levels=[0,fuzzy_thresh,2], colors=None, hatches=['','//'], alpha=0)
+
 
 #
-# with hatches...
 
-# TODO: hatches don't seem to respect the linear interpolation 
-# used by the rest of the contour tools. Can I get this to work?
-
-#ax.tricontourf(df['lambda'],df['q'],np.logical_and(-0.1<df['skew_l'],df['skew_l']<0.1), levels=[0,1,2], vmin=-0.15,vmax=0.15, colors=None, hatches=['','\\\\'], alpha=0)
-
-#ax.tricontourf(df['lambda'],df['q'],np.logical_and(-0.1<df['skew_g'],df['skew_g']<0.1), levels=[0,1,2], vmin=-0.15,vmax=0.15, colors=None, hatches=['','//'], alpha=0)
-
-fig.savefig('short_long_intersection.pdf',
-metadata = {
-    'Creator' : __file__,
-    'Author': 'Manuchehr Aminian'
-    }
-)
+if False:
+    fig.savefig('short_long_intersection.pdf',
+    metadata = {
+        'Creator' : __file__ + " using data " + data_file_basename,
+        'Author': 'Manuchehr Aminian'
+        }
+    )
 
 fig.show()
 
